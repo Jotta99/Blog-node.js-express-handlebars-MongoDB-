9989a2bb7d30b4b91c8cb5a5391c6a75309fa85d
Jotta99 <joasamorim611@gmail.com>
2023-11-13 17:45:48 -0400

    update

--- models/Postagem.js
+++ models/Postagem.js
@@ -1,6 +1,54 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+
+// Config Date
+
+var date = new Date()
+var day = date.getDate()
+var month = date.getMonth() + 1;
+var year = date.getFullYear()
+var arrMonth = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+if(month === 1){
+month = arrMonth[0]
+}
+if(month === 2){
+  month = arrMonth[1]
+}
+if(month === 3){
+  month = arrMonth[2]
+}
+if(month === 4){
+  month = arrMonth[3]
+}
+if(month === 5){
+  month = arrMonth[4]
+}
+if(month === 6){
+  month = arrMonth[5]
+}
+if(month === 7){
+  month = arrMonth[6]
+}
+if(month === 8){
+  month = arrMonth[7]
+}
+if(month === 9){
+  month = arrMonth[8]
+}
+if(month === 10){
+  month = arrMonth[9]
+}
+if(month === 11){
+  month = arrMonth[10]
+}
+if(month === 12){
+  month = arrMonth[11]
+}
+
+const atualDate = `${day} de ${month} de ${year}`
+
 const Postagem = new Schema({
     titulo: {
         type: String,
@@ -24,8 +72,8 @@ const Postagem = new Schema({
         require: true
     },
     date: {
-        type: Date,
-        default: Date.now(),
+        type: String,
+        default: atualDate,
     },
 })
 
